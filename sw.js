82fd4f144d3ce8d7e1036c54f6f6b00e04fb1989
jarf0669-ship-it <jarf0669@gmail.com>
2026-09-24 11:26:53 +0200

    correciones fichero

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'amora-pwa-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
